5f3b594f181d615ff4fa6bd3807b09a4107ac7c0
Vinit <vinits1014@gmail.com>
2024-06-01 15:45:40 +0530

    Hello

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 import { PrismaClient } from "@prisma/client";
-const prisma = require("@prisma/client");
+const prisma = new PrismaClient();
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
