a0867074214ed34657f7a924f767314e44e7d811
Vinit <vinits1014@gmail.com>
2024-06-02 11:27:49 +0530

    Updated tsconfig.json file

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,14 @@
 import { prisma } from "./prisma" // import your extended Prisma Client instance
 
 async function main() {
-  const stream = await prisma.notification.stream({ name: 'notification-stream'})
 
-  for await (const event of stream) {
-    console.log('New event:', event)
-  }
+    const subscription = await prisma.players.subscribe()
 }
 
 main()
+
+//   const stream = await prisma.players.stream({ name: 'notification-stream'})
+
+//   for await (const event of stream) {
+//     console.log('New event:', event)
+//   }
