fbf8e13dfefc42f98ca18b40099529d31ee312f1
Vinit <vinits1014@gmail.com>
2024-06-05 14:36:02 +0530

    Completed with leaderboard

--- backend/index.ts
+++ backend/index.ts
@@ -1,34 +1,89 @@
+// import http from "http";
+// import { Server } from "socket.io";
+// import { prisma } from "./prisma";
+
+// const PORT = process.env.PORT || 8000;
+
+// const httpServer = http.createServer((req, res) => {
+//     // Define the routes
+//     if (req.method === "GET" && req.url === "/") {
+//       res.writeHead(200, { "Content-Type": "text/plain" });
+//       res.end("Server is running ...");
+//     } else {
+//       // Handle 404 Not Found
+//       res.writeHead(404, { "Content-Type": "text/plain" });
+//       res.end("404 Not Found");
+//     }
+// });
+
+// const corsOrigins = [
+//     process.env.CLIENT_URL ?? "http://localhost:3000",
+//     "http://localhost:3000",
+// ]
+// console.log(`cors origins: `, corsOrigins)
+
+// const io = new Server(httpServer, {
+//     cors: {
+//       origin: corsOrigins,
+//       methods: ["GET", "POST"],
+//       credentials: true,
+//     },
+// });
+
+// httpServer.listen(PORT, async () => {
+//   console.log(`socket.io server is running on port ${PORT}`);
+//   await main(io);
+// });
+
+// process.on('SIGINT', () => {
+//   process.exit(0)
+// })
+
+// async function main(io:Server) {
+//   const stream = await prisma.players.stream()
+
+//   process.on('exit', (code) => {
+//     stream.stop()
+//   })
+
+//   for await (const event of stream) {
+//     console.log('just received an event:', event)
+
+//     if (event.action === 'update') {
+//         io.sockets.emit("player_points",event);
+//     }
+//   }
+// }
+
+
 import http from "http";
 import { Server } from "socket.io";
 import { prisma } from "./prisma";
 
-
 const PORT = process.env.PORT || 8000;
 
 const httpServer = http.createServer((req, res) => {
-    // Define the routes
-    if (req.method === "GET" && req.url === "/") {
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("Server is running ...");
-    } else {
-      // Handle 404 Not Found
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      res.end("404 Not Found");
-    }
+  if (req.method === "GET" && req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Server is running ...");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Not Found");
+  }
 });
 
-const corsOrigins = [
-    process.env.CLIENT_URL ?? "http://localhost:3000",
-    "http://localhost:3000",
-]
-console.log(`cors origins: `, corsOrigins)
+// const corsOrigins = [
+//   process.env.CLIENT_URL ?? "http://localhost:3000",
+//   "http://localhost:3000",
+// ];
+// console.log(`cors origins: `, corsOrigins);
 
 const io = new Server(httpServer, {
-    cors: {
-      origin: corsOrigins,
-      methods: ["GET", "POST"],
-      credentials: true,
-    },
+  cors: {
+    origin: '*',
+    methods: ["GET", "POST"],
+    // credentials: true,
+  },
 });
 
 httpServer.listen(PORT, async () => {
@@ -36,23 +91,22 @@ httpServer.listen(PORT, async () => {
   await main(io);
 });
 
-process.on('SIGINT', () => {
-  process.exit(0)
-})
+process.on("SIGINT", () => {
+  process.exit(0);
+});
 
-async function main(io:Server) {
-  const stream = await prisma.players.stream()
+async function main(io: Server) {
+  const stream = await prisma.players.stream();
 
-  process.on('exit', (code) => {
-    stream.stop()
-  })
+  process.on("exit", () => {
+    stream.stop();
+  });
 
   for await (const event of stream) {
-    console.log('just received an event:', event)
+    console.log("just received an event:", event);
 
-    if (event.action === 'update') {
-        io.sockets.emit("player_points",event);
+    if (event.action === "update") {
+      io.sockets.emit("player_points", event);
     }
   }
 }
-
--- frontend/components/LeaderBoard.tsx
+++ frontend/components/LeaderBoard.tsx
@@ -1,3 +1,141 @@
+// "use client"
+
+// import { useState, useEffect, MutableRefObject, useRef } from "react";
+// import FlipMove from "react-flip-move";
+// import { io, Socket } from "socket.io-client";
+
+// const LeaderBoard = ({ initialPlayers }: { initialPlayers: any }) => {
+
+//   const [players, setPlayers] = useState(initialPlayers || []);
+//   const [updatedPlayerId, setUpdatedPlayerId] = useState<number | null>(null);
+  
+//   let socketRef: MutableRefObject<Socket | null> = useRef(null);
+  
+//   useEffect(()=>{
+//     const updatePoints = (updatedPlayer:any)=>{
+//       setPlayers((prevPlayers:any) =>
+//         prevPlayers.map((player:any) =>
+//           player.id === updatedPlayer.id ? updatedPlayer : player
+//         )
+//       );
+//       setUpdatedPlayerId(updatedPlayer.id);
+//       setTimeout(()=>setUpdatedPlayerId(null),1000);
+//     }
+
+//     const url = process.env.SERVER_URL ?? `http://localhost:8000`;
+//     socketRef.current = io(url);
+
+//     socketRef.current.on("player_points",(event:any)=>{
+//       console.log("recieves UPDATE event from server", event);
+//       updatePoints(event.after)
+      
+//     })
+
+//     return ()=>{
+//       socketRef.current?.off("player_points");
+//     }
+//   },[players]);
+
+//   const sortedPlayers = [...players].sort((a,b)=> b.points - a.points);
+
+//   return (
+//     <div className="w-full p-4">
+//       <p className="text-center text-gray text-xl font-semibold mb-4">
+//         🏆 Welcome to the Real-Time Leaderboard 🏆
+//       </p>
+//       <FlipMove>
+//         {sortedPlayers.map((player,i)=>(
+//           <div
+//           key={player.id}
+//           className={`flex items-center justify-between p-2 mb-2 rounded-md shadow-md relative ${
+//             updatedPlayerId === player.id ? 'blink' : 'bg-white'
+//           }`}
+//           >
+//             <div className="text-lg font-semibold text-gray-800">
+//               {i === 0 ? `${player.name} 🥇` : player.name}
+//             </div>
+//             <div className="text-lg font-semibold text-gray-600">
+//               {player.points}
+//             </div>
+//           </div>
+//         ))}
+//       </FlipMove>
+//     </div>
+//   )
+// }
+
+// export default LeaderBoard;
+
+// "use client"
+
+// import { useState, useEffect, MutableRefObject, useRef } from "react";
+// import FlipMove from "react-flip-move";
+// import { io, Socket } from "socket.io-client";
+
+// const LeaderBoard = ({ initialPlayers }: { initialPlayers: any }) => {
+//   const [players, setPlayers] = useState(initialPlayers || []);
+//   const [updatedPlayerId, setUpdatedPlayerId] = useState<number | null>(null);
+  
+//   let socketRef: MutableRefObject<Socket | null> = useRef(null);
+
+//   useEffect(()=>{
+//     console.log(players);
+//   },[players])
+  
+//   useEffect(() => {
+//     const updatePoints = (updatedPlayer:any) => {
+//       setPlayers((prevPlayers:any) =>
+//         prevPlayers.map((player:any) =>
+//           player.id === updatedPlayer.id ? updatedPlayer : player
+//         )
+//       );
+//       setUpdatedPlayerId(updatedPlayer.id);
+//       setTimeout(() => setUpdatedPlayerId(null), 1000);
+//     };
+
+//     const url = process.env.SERVER_URL ?? `http://localhost:8000`;
+//     socketRef.current = io(url);
+
+//     socketRef.current.on("player_points", (event:any) => {
+//       console.log("received UPDATE event from server", event);
+//       updatePoints(event.after);
+//     });
+
+//     return () => {
+//       socketRef.current?.off("player_points");
+//     };
+//   }, []); // Empty dependency array ensures this runs only once
+
+//   const sortedPlayers = [...players].sort((a,b) => b.points - a.points);
+
+//   return (
+//     <div className="w-full p-4">
+//       <p className="text-center text-gray text-xl font-semibold mb-4">
+//         🏆 Welcome to the Real-Time Leaderboard 🏆
+//       </p>
+//       <FlipMove>
+//         {sortedPlayers.map((player,i) => (
+//           <div
+//             key={player.id}
+//             className={`flex items-center justify-between p-2 mb-2 rounded-md shadow-md relative ${
+//               updatedPlayerId === player.id ? 'blink' : 'bg-white'
+//             }`}
+//           >
+//             <div className="text-lg font-semibold text-gray-800">
+//               {i === 0 ? `${player.name} 🥇` : player.name}
+//             </div>
+//             <div className="text-lg font-semibold text-gray-600">
+//               {player.points}
+//             </div>
+//           </div>
+//         ))}
+//       </FlipMove>
+//     </div>
+//   );
+// };
+
+// export default LeaderBoard;
+
 "use client"
 
 import { useState, useEffect, MutableRefObject, useRef } from "react";
@@ -5,58 +143,67 @@ import FlipMove from "react-flip-move";
 import { io, Socket } from "socket.io-client";
 
 const LeaderBoard = ({ initialPlayers }: { initialPlayers: any }) => {
-
   const [players, setPlayers] = useState(initialPlayers || []);
   const [updatedPlayerId, setUpdatedPlayerId] = useState<number | null>(null);
   
   let socketRef: MutableRefObject<Socket | null> = useRef(null);
   
-  useEffect(()=>{
-    setPlayers((prevPlayers) =>
-      prevPlayers.map((player) =>
-        player.id === updatedPlayer.id ? updatedPlayer : player
-      )
-    );
-    setUpdatedPlayerId(updatedPlayer.id);
-  },[]);
+  useEffect(() => {
+    const updatePoints = (updatedPlayer:any) => {
+      setPlayers((prevPlayers:any) =>
+        prevPlayers.map((player:any) =>
+          player.id === updatedPlayer.id ? updatedPlayer : player
+        )
+      );
+      setUpdatedPlayerId(updatedPlayer.id);
+      setTimeout(() => setUpdatedPlayerId(null), 1000);
+    };
+
+    const url = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:8000';
+    socketRef.current = io(url);
+
+    socketRef.current.on("connect_error", (err) => {
+      console.error(`Connection error: ${err.message}`);
+    });
+
+    socketRef.current.on("player_points", (event:any) => {
+      console.log("received UPDATE event from server", event);
+      updatePoints(event.after);
+    });
+
+    return () => {
+      socketRef.current?.off("player_points");
+    };
+  }, [players]); // Empty dependency array ensures this runs only once
   
+  const sortedPlayers = [...players].sort((a,b) => b.points - a.points);
+
   return (
     <div className="w-full p-4">
       <p className="text-center text-gray text-xl font-semibold mb-4">
         🏆 Welcome to the Real-Time Leaderboard 🏆
       </p>
       <FlipMove>
-        <div className='flex items-center justify-between p-2 mb-2 rounded-md shadow-md relative'>
-            <div className="text-lg font-semibold text-gray-800">Username 1</div>
-            <div className="text-lg font-semibold text-gray-600">289</div>
-        </div>
-        <div className='flex items-center justify-between p-2 mb-2 rounded-md shadow-md relative'>
-            <div className="text-lg font-semibold text-gray-800">Username 2</div>
-            <div className="text-lg font-semibold text-gray-600">250</div>
-        </div>
-        <div className='flex items-center justify-between p-2 mb-2 rounded-md shadow-md relative'>
-            <div className="text-lg font-semibold text-gray-800">Username 3</div>
-            <div className="text-lg font-semibold text-gray-600">300</div>
-        </div>
-        </FlipMove>
-        </div>
-    )
-}
-
-export default LeaderBoard
-
-{/* {sortedPlayers.map((player, i) => (
-  <div
-    key={player.id}
-    className={`flex items-center justify-between p-2 mb-2 rounded-md shadow-md relative ${
-      updatedPlayerId === player.id ? 'blink' : 'bg-white'
-    }`}
-  >
-    <div className="text-lg font-semibold text-gray-800">
-      {i === 0 ? `${player.username} 🥇` : player.username}
+        {sortedPlayers.map((player,i) => (
+          <div
+            key={player.id}
+            className={`flex items-center justify-between p-2 mb-2 rounded-md shadow-md relative ${
+              updatedPlayerId === player.id ? 'blink' : 'bg-white'
+            }`}
+          >
+            <div className="text-lg font-semibold text-gray-800">
+              {i === 0 ? `${player.name} 🥇` : player.name}
+            </div>
+            <div className="text-lg font-semibold text-gray-600">
+              {player.points}
+            </div>
+          </div>
+        ))}
+      </FlipMove>
     </div>
-    <div className="text-lg font-semibold text-gray-600">
-      {player.points}
-    </div>
-  </div>
-))} */}
+  );
+};
+
+export default LeaderBoard;
+
+
