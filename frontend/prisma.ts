cd55708af21e104c4248c44fbb8ba2aec4dff6bb
Vinit <vinits1014@gmail.com>
2024-07-05 00:04:20 +0530

    Done

--- frontend/prisma.ts
+++ frontend/prisma.ts
@@ -1,11 +1,3 @@
 import { PrismaClient } from '@prisma/client'
-import { withPulse } from '@prisma/extension-pulse'
-
 const prisma = new PrismaClient()
-  .$extends(
-    withPulse({
-      apiKey: process.env['PULSE_API_KEY'] as string
-    })
-  )
-
 export { prisma }
