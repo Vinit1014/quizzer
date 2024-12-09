032091af3d2c195d3312851c98246d24d3eb70cc
Vinit <vinits1014@gmail.com>
2024-12-09 14:10:56 +0530

    login_1

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,12 +1,9 @@
-'use client'
 import Login from '@/components/Login'
-
 
 export default function Page() {
   return (
     <div>
-        {/* <Login/> */}
-        <h1>Hello Login</h1>
+        <Login/>
     </div>
   )
 }
--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -1,11 +1,9 @@
-'use client'
 import SignUp from '@/components/SignUp';
 
 export default function Page() {
   return (
     <div>
-        {/* <SignUp/> */}
-        <h1>Hello Signup</h1>
+        <SignUp/>
     </div>
   )
 }
