0002d2bc780b7d898f491791d82edec3dcc37550
Vinit <vinits1014@gmail.com>
2025-04-27 12:59:49 +0530

    Compiled the player type error

--- frontend/app/api/join/route.ts
+++ frontend/app/api/join/route.ts
@@ -1,57 +1,3 @@
-// import { NextResponse } from "next/server";
-// import {prisma} from "@backend/prisma";
-
-// export async function POST(req:Request){
-//     try{
-//         const body = await req.json();
-//         const {roomName, playerName} = body;
-//         console.log(roomName);
-        
-//         const room = await prisma.room.findFirst({
-//             where:{
-//                 roomName: roomName,
-//             }
-//         })
-
-//         if (!room){
-//             return NextResponse.json({error: "Room not found"},{ status: 404});
-//         }
-        
-//         const player = await prisma.player.create({
-//             data:{
-//                 name:playerName,
-//                 points:0,
-//                 roomId: room.id,
-//             }
-//         })
-        
-//         const updatedRoom = await prisma.room.update({
-//             where: {
-//                 id: room.id,
-//             },
-//             data: {
-//                 players: {
-//                     connect:{
-//                         id: player.id,
-//                     },
-//                 },
-//             },
-//             include:{
-//                 players: true,  
-//             },
-//         });
-        
-//         return NextResponse.json({data:player});
-//         // return NextResponse.json({data:"Success"});
-//         // console.log("Successfull");
-    
-//     }catch(error){
-//         // console.log(error);
-//         return NextResponse.json({ error: error }, { status: 500 });
-//         // return NextResponse.json({ erro  r: 'An error occurred while creating the room and player' }, { status: 500 });
-//     }
-// }
-
 
 import { NextResponse } from "next/server";
 import { prisma } from "@/prisma";
@@ -83,7 +29,7 @@ export async function POST(req: Request) {
     }
 
     // Check if the player already exists in the room
-    let player = room.players.find((player) => player.userId === userId);
+    let player = room.players.find((player: any) => player.userId === userId);
 
     // If the player doesn't exist, create a new player and add to the room
     if (!player) {
